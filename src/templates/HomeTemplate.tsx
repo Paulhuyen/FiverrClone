787ebfe69c645d11446165c3a56cf78aaa890e9a
Huyen <paul.huyentran0@gmail.com>
2022-10-09 17:46:32 +0700

    setup Fiverr

---
+++ src/templates/HomeTemplate.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+
+type Props = {}
+
+export default function HomeTemplate({}: Props) {
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
