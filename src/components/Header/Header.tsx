8600b98fd0d63610fd85e428d032233418bdd78f
Tuyet-Vy <nhuochanvu@gmail.com>
2022-10-25 00:05:42 +0700

    first update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,52 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 type Props = {
-  title?:string
-}
+  title?: string;
+};
 
 export default function Header(props: Props) {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
-  <div className="collapse navbar-collapse" id="collapsibleNavId">
-    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-      <li className="nav-item">
-        <NavLink className="nav-link active" to="/" aria-current="page">Home <span className="visually-hidden">(current)</span></NavLink>
-      </li>
-    </ul>
-    <form className="d-flex my-2 my-lg-0">
-      <input className="form-control me-sm-2" type="text" placeholder="Search" />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+      <NavLink className="navbar-brand d-flex" to="/home">
+        <h4>fiverr</h4>
+        <span>
+          <i className="fa fa-circle" aria-hidden="true"></i>
+        </span>
+      </NavLink>
 
-  )
+      {/* <button
+                className="navbar-toggler d-lg-none"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            /> */}
+
+      <div className="collapse navbar-collapse" id="collapsibleNavId">
+        <ul className="navbar-nav mt-2 mt-lg-0">
+          <li className="nav-item">
+            <NavLink className="nav-link active" to="/" aria-current="page">
+              Become a Seller <span className="visually-hidden">(current)</span>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Sign in
+            </NavLink>
+          </li>
+          <li>
+            <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="submit"
+            >
+              Join
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
